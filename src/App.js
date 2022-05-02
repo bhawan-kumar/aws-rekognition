bfb57ccb883fecec6e45073e16f633b9389280c9
bhawan-kumar <chinavada.4444@gmail.com>
2022-05-02 12:12:17 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({file: null})
+
+  const upload = () => {
+    if(!data.file) {
+      alert("Upload any file to recognise...")
+      return
+    }
+    const formData = new FormData()
+    formData.add("image", data.file)
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mb-3">
+        <label htmlFor="formFile" className="form-label">Upload any file</label>
+        <input className="form-control" type="file" id="formFile" onChange={e => setData({file: e.target.files[0]})}/>
+      </div>
+      <button className='btn btn-dark'>Upload</button>
     </div>
   );
 }
---
+++ src/aws/index.js
@@ -0,0 +1,8 @@
+import {Rekognition, config, Config} from "aws-sdk"
+
+Config.update({
+
+})
+
+const rekognition = new Rekognition()
+rekognition
